9c8f3ef2176e45b338478f10594707c98008b0e6
aditya channe <adichanne@yahoo.in>
2020-06-05 00:08:35 +0530

    Name Error fixed
    done

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Aditya Chenne'
+        name: 'Aditya Channe'
     })
 })
 
